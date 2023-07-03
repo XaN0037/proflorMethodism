091ebefd4f865671da5e553319619babc260c39f
XaN0037 <xannodirbek@gmail.com>
2023-07-03 17:16:37 +0500

    asd

--- dashboard/v1/methods/patient.py
+++ dashboard/v1/methods/patient.py
@@ -48,23 +48,23 @@ def patient_add(requests, params):
 
 def patient_change(requests, params):
     try:
+
         patient = Patient.objects.filter(pk=params['id']).first()
     except:
         return custom_response(False, message=MESSAGE["NotData"])
     if patient:
-        patient.name = params['name'] if params['name'] else patient.name
-        patient.first_name = params['first_name'] if params['first_name'] else patient.first_name
-        patient.father_name = params['father_name'] if params['father_name'] else patient.father_name
-        patient.age = params['age'] if params['age'] else patient.age
-        patient.phone = params['phone'] if params['phone'] else patient.phone
-        patient.comment = params['comment'] if params['comment'] else patient.comment
+        patient.name = params.get('name', patient.name)
+        patient.first_name = params.get('first_name', patient.first_name)
+        patient.father_name = params.get('father_name', patient.father_name)
+        patient.age = params.get('age', patient.age)
+        patient.phone = params.get('phone', patient.phone)
+        patient.comment = params.get('comment', patient.comment)
 
         patient.save()
 
         return custom_response(True, patient_format_one(patient))
     else:
         return custom_response(False, message=MESSAGE['UndefinedError'])
-
 
 def patient_delete(requests, params):
     try:
